0de1dd701b294938751dce8a73e96234ad3d1926
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-05-04 17:48:01 +0200

    Made databaseContext, Added nuget packages (Sqlite, entity framework, swagger, logger)

--- MechanicAPI/Program.cs
+++ MechanicAPI/Program.cs
@@ -1,9 +1,40 @@
+using MechanicAPI.DB;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 namespace MechanicAPI;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddControllers();
+
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<MechanicDataContext>(
+            options =>
+            {
+                options.UseSqlite(builder.Configuration.GetConnectionString("SQLLiteConnection"));
+                options.UseLazyLoadingProxies();
+            });
+        
+        builder.Services.AddSerilog(
+            options => options
+                .MinimumLevel.Information()
+                .WriteTo.Console());
+        
+        //Singleton
+        
+        //build
+        
+        //if is development use swagger and ui
+        
+        //app. usehttpsRedirection
+        // ap.MapControllers
+        //app.run
         
     }
 }
---
+++ MechanicAPI/Services/ClientService.cs
@@ -0,0 +1,32 @@
+using MechanicAPI.Classes;
+using MechanicAPI.Interfaces;
+
+namespace MechanicAPI.Services;
+
+public class ClientService: IClientService
+{
+    public void Add(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Client Get(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Client> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
