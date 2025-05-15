257e8a2635b3aaf2c554444be5c65d11d1e24121
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-05-15 12:41:37 +0200

    feat(ui): added empty client and work subpages for implementation in the future

--- MechanicAPI/obj/Debug/net9.0/MechanicAPI.AssemblyInfo.cs
+++ MechanicAPI/obj/Debug/net9.0/MechanicAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechanicAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3aaae0000f580d4dbb9ebf94748dcb1db8960e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a413b0a12b8aff020395e7ec175f608a9bb5d329")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechanicAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechanicAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
